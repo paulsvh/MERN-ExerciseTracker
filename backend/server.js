a4deecd9700920a399d055d79db8b103d29bb5bf
paul_vonhagen <pvonhagen26@gmail.com>
2021-04-12 18:48:54 -0700

    fix imports on server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import express, { json } from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
