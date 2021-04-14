62228f80180916f9f7b5d7ecda73c15a2bafb3d0
paul_vonhagen <pvonhagen26@gmail.com>
2021-04-13 20:13:35 -0700

    fix typo in users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,7 +7,7 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-rotuer.route('/add').post((req, res) => {
+router.route('/add').post((req, res) => {
     const username = req.body.username;
 
     const newUser = new user({username});
